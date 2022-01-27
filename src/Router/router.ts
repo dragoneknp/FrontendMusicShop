e62bd0c37bd14f4617cb1c603e85078cf22dc294
Dragonek <dragoneknp@gmail.com>
2022-01-27 19:24:49 +0300

    refactor: added router

---
+++ src/Router/router.ts
@@ -0,0 +1,72 @@
+import About from "../Pages/About/about";
+import CardDetails from "../Pages/CardDetails/cardDetails";
+import Discover from "../Pages/Discover/discover";
+import Error from "../Pages/Error/error";
+import FAQ from "../Pages/FAQ/faq";
+import Home from "../Pages/Home/home";
+import Login from "../Pages/Login/login";
+import Marketplace from "../Pages/Marketplace/marketplace";
+import MarketplaceListenings from "../Pages/MarketplaceListenings/marketplaceListenings";
+import Terms from "../Pages/Terms/terms";
+export const router = [
+    {
+        name: "Discover",
+        path: "/discover",
+        element: Discover,
+    },
+    {
+        name: "Discover Card",
+        path: "/discover/:id",
+        element: CardDetails,
+    },
+    {
+        name: "Marketplace",
+        path: "/marketplace",
+        element: Marketplace,
+    },
+    {
+        name: "Marketplace Card",
+        path: "/marketplace/:id",
+        element: MarketplaceListenings,
+    },
+    {
+        name: "Home",
+        path: "/home",
+        element: Home,
+    },
+    {
+        name: "Home Card",
+        path: "/home/:id",
+        element: CardDetails,
+    },
+    {
+        name: "Login",
+        path: "/login",
+        element: Login,
+    },
+    {
+        name: "Terms",
+        path: "/terms",
+        element: Terms,
+    },
+    {
+        name: "Error",
+        path: "/error",
+        element: Error,
+    },
+    {
+        name: "About",
+        path: "/about",
+        element: About,
+    },
+    {
+        name: "FAQ",
+        path: "/FAQ",
+        element: FAQ,
+    },
+    {
+        name: "Page not found",
+        path: "/error",
+        element: Error,
+    },
+];
--- src/index.tsx
+++ src/index.tsx
@@ -7,38 +7,21 @@ import {
 } from "react-router-dom";
 import ScrollToTop from "./Components/ScrollToTop/scrollToTop";
 import "./index.css";
-import About from "./Pages/About/about";
-import CardDetails from "./Pages/CardDetails/cardDetails";
-import Discover from "./Pages/Discover/discover";
-import Error from "./Pages/Error/error";
-import FAQ from "./Pages/FAQ/faq";
-import Home from "./Pages/Home/home";
-import Login from "./Pages/Login/login";
-import Marketplace from "./Pages/Marketplace/marketplace";
-import MarketplaceListenings from "./Pages/MarketplaceListenings/marketplaceListenings";
-import Terms from "./Pages/Terms/terms";
 import { store } from "./Store/store";
 import { Provider } from "react-redux";
+import { router } from "./Router/router";
 ReactDOM.render(
     <Provider store={store}>
         <Router>
             <ScrollToTop />
             <Routes>
-                <Route path="/discover" element={<Discover />} />
-                <Route path="/discover/:id" element={<CardDetails />} />
-                <Route path="/marketplace" element={<Marketplace />} />
-                <Route
-                    path="/marketplace/:id"
-                    element={<MarketplaceListenings />}
-                />
-                <Route path="/home" element={<Home />} />
-                <Route path="/home/:id" element={<CardDetails />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/terms" element={<Terms />} />
-                <Route path="/error" element={<Error />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/FAQ" element={<FAQ />} />
-                <Route path="*" element={<Navigate replace to="/error" />} />
+                {router.map((route) => (
+                    <Route
+                        key={route.name}
+                        path={route.path}
+                        element={<route.element />}
+                    />
+                ))}
             </Routes>
         </Router>
     </Provider>,
